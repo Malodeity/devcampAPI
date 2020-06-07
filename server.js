9a49f5543b9b0bb30509280ffd12497f51fcc0bd
Malo <malomthethwa@outlook.com>
2020-06-06 19:09:06 -0500

    Section 2 Done

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan'); //for logging api's
 
 //Route Files
-
 const bootcamps = require('./routes/bootcamps');
 
 //load the env file
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Dev Logging Middleware - running it only on dev
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mount Routes
 app.use('/api/v1/bootcamps', bootcamps);
