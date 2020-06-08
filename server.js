72b32940fa61120618bda830ab620ead82be6f14
Malo <malomthethwa@outlook.com>
2020-06-08 08:57:00 +0200

    Database Connection Created

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan'); //for logging api's
-
-//Route Files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //load the env file
 dotenv.config({ path: './config/config.env' });
+
+//Connect To DB
+connectDB();
+
+//Route Files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -19,7 +24,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+//Handle unhanlded promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  server.close(() => process.exit(1));
+});
