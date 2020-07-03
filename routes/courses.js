cb95f237b3ed596be4d1723f4917e55462740654
Malo <malomthethwa@outlook.com>
2020-07-03 10:51:19 +0200

    Update and Delete Course API added

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {
-  getCourses
+  getCourses, getCourse, addCourse, updateCourse, deleteCourse
 } = require('../controllers/courses');
 
 const router = express.Router({mergeParams: true}); //Merging only for the bootcamp route to work with the courses to get the courses linked to a certain bootcamp
 
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
