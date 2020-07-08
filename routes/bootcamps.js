d5b629a2f57599406f95abc925be50abe0d629d2
Malo <malomthethwa@outlook.com>
2020-07-08 15:18:35 +0200

    Role Autherazation following the protecting of routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -18,7 +18,7 @@ const courseRouter = require('./courses');
 
 const router = express.Router();
 
-const {protect } = require('../middleware/auth'); //user must be logged in to use a protected route
+const {protect, authorize } = require('../middleware/auth'); //user must be logged in to use a protected route
 
 //Re-routing into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
@@ -27,16 +27,16 @@ router.use('/:bootcampId/courses', courseRouter);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 //for file upload
-router.route('/:id/photo').put(protect ,bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, authorize('publisher', 'admin') ,bootcampPhotoUpload);
 
 //Only for the routes that doesn't need params
-router.route('/').get(advancedResults(Bootcamp, 'courses'),getBootcamps).post(protect,createBootcamps); //adding the advanced results middleware to the get bootcamp route
+router.route('/').get(advancedResults(Bootcamp, 'courses'),getBootcamps).post(protect,authorize('publisher', 'admin'),createBootcamps); //adding the advanced results middleware to the get bootcamp route
 
 //Only for the routes than needs the same param such as ID
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamps)
-  .delete(protect, deleteBootcamps);
+  .put(protect,authorize('publisher', 'admin'),updateBootcamps)
+  .delete(protect,authorize('publisher', 'admin'), deleteBootcamps);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -9,12 +9,12 @@ const advancedResults = require('../middleware/advancedResults');
 const router = express.Router({mergeParams: true}); //Merging only for the bootcamp route to work with the courses to get the courses linked to a certain bootcamp
 
 
-const {protect } = require('../middleware/auth'); //user must be logged in to use a protected route.
+const {protect, authorize } = require('../middleware/auth'); //user must be logged in to use a protected route.
 
 router.route('/').get(advancedResults(Course, {
   path: 'bootcamp',
   select : 'name description'
-}),getCourses).post(protect,addCourse);
-router.route('/:id').get(getCourse).put(protect,updateCourse).delete(protect,deleteCourse);
+}),getCourses).post(protect,authorize('publisher', 'admin'),addCourse);
+router.route('/:id').get(getCourse).put(protect,authorize('publisher', 'admin'),updateCourse).delete(protect,authorize('publisher', 'admin'),deleteCourse);
 
 module.exports = router;
